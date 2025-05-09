41d8836088c9799c02e9937450751a3fb4b86286
Methmal-Deshapriya <methmaldeshapriya2002@gmail.com>
2025-05-09 15:36:17 +0530

    chore: remove dist folder from Git and ignore it

--- aidf-back-end/dist/infrastructure/stripe.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const stripe_1 = require("stripe");
-const stripe = new stripe_1.Stripe(process.env.STRIPE_SECRET_KEY);
-exports.default = stripe;
-//# sourceMappingURL=stripe.js.map
