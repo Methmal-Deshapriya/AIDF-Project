3fbe3eb904d859f6cb03aa3ac58db513c0769282
Methmal-Deshapriya <methmaldeshapriya2002@gmail.com>
2025-05-15 19:07:59 +0530

    missing BACKEND_URL was added in api.js the error was BACKEND_URL is defined without const keyword

--- aidf-front-end/reactapp/src/lib/api.js
+++ aidf-front-end/reactapp/src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = createApi({
   reducerPath: "api",
