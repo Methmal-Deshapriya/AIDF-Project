a0938e1b18a1abc9b5e0abc8e1ec6583d48936e4
Methmal-Deshapriya <methmaldeshapriya2002@gmail.com>
2025-05-15 19:40:25 +0530

    destructured error is a object and cannot render a object directly so corrected it

--- aidf-front-end/reactapp/src/components/ui/HotelListing.jsx
+++ aidf-front-end/reactapp/src/components/ui/HotelListing.jsx
@@ -109,7 +109,9 @@ const HotelListing = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8 ">
-          <div className="text-red-500">{error}</div>
+          <div className="text-red-500">
+            {error.data || error.error || "Something went wrong"}
+          </div>
         </div>
       </div>
     );
